3144e7fed7ad26814d4e87982dd9b1bcb2dd9e99
dpuentex <dpuente1097@gmail.com>
2024-05-10 21:58:14 -0700

    bootstrapp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import './App.css';
+import "./App.css";
+import React from "react";
+import TopBar from "./components/TopBar";
+import Background from "./components/Background";
+import StoreListings from "./components/StoreListing";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <TopBar />
+      <Background />
+      <StoreListings />
+    </>
   );
 }
 
